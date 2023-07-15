ad89f7b12a5e2e7bd1e40c9999c56df4a2c0f321
SajjadSadeh <136505511+SajjadSadeh@users.noreply.github.com>
2023-07-15 14:13:57 +0330

    Adding the initial files of the project - the first version of the codes

---
+++ app.js
@@ -0,0 +1,61 @@
+let r = document.querySelector(':root');
+let changModeBtn = document.querySelector("#changModeBtn")
+let moonAndSunImg = document.querySelector("#moonAndSun")
+let openMenuBtn = document.querySelector(".menu-bar-icon")
+let closeMenuBtn = document.querySelector(".menu-close-btn")
+
+let width = 0
+
+let flag = true
+
+function openMenu() {
+    document.querySelector(".nav-btns").style.marginLeft = "0"
+    document.querySelector("#Screen-blur").style.display = "block"
+
+}
+function closeMenu() {
+    document.querySelector(".nav-btns").style.marginLeft = "500px"
+    document.querySelector("#Screen-blur").style.display = "none"
+}
+// change mode
+changModeBtn.addEventListener("click", function () {
+    if (flag) {
+        flag = false
+        moonAndSunImg.setAttribute("src", "./images/sun.png")
+        moonAndSunImg.setAttribute("fill", "white")
+        r.style.setProperty('--white', '#1D1D1D');
+        r.style.setProperty('--titleColor', '#FFF');
+        r.style.setProperty('--primary', '#588DF9');
+        r.style.setProperty('--lightGrey', '#242424');
+        r.style.setProperty('--subtitleColor', '#B1B1B1');
+        r.style.setProperty('--green', '#00ac4f');
+
+        // change menu bar white to black
+        document.querySelector(".menu-bar-icon").setAttribute("src", "./images/bar white.png")
+
+    } else {
+        flag = true
+        moonAndSunImg.setAttribute("src", "./images/moon.png")
+        r.style.setProperty('--white', '#fff');
+        r.style.setProperty('--titleColor', '#000f33');
+        r.style.setProperty('--primary', '#2702ff');
+        r.style.setProperty('--lightGrey', '#f8f8f8');
+        r.style.setProperty('--subtitleColor', '#656c80');
+        r.style.setProperty('--green', '#00ac4f');
+
+        // change menu bar white to black
+        document.querySelector(".menu-bar-icon").setAttribute("src", "./images/bar balck.png")
+    }
+})
+
+openMenuBtn.addEventListener("click", function () {
+    openMenu()
+})
+closeMenuBtn.addEventListener("click", function () {
+    closeMenu()
+
+})
+
+window.addEventListener('resize', function () {
+    closeMenu()
+});
